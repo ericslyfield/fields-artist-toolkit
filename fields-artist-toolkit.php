3480f103dbd5e22622db8e04f396ed9bf27e1d52
Sly <eric.slyfield@gmail.com>
2026-04-20 21:39:57 -0400

    feat: add medium taxonomy

--- fields-artist-toolkit.php
+++ fields-artist-toolkit.php
@@ -47,3 +47,60 @@ function register_post_type_work() {
     register_post_type('work', $args);
 }
 add_action('init', 'Nonarchival\FieldsToolkit\register_post_type_work');
+
+// Register Medium Taxonomy
+function register_taxonomy_medium() {
+    $labels = array(
+        'name'              => __('Mediums', 'nonarchival'),
+        'singular_name'     => __('Medium', 'nonarchival'),
+        'search_items'      => __('Search Mediums', 'nonarchival'),
+        'all_items'         => __('All Mediums', 'nonarchival'),
+        'edit_item'         => __('Edit Medium', 'nonarchival'),
+        'update_item'       => __('Update Medium', 'nonarchival'),
+        'add_new_item'      => __('Add New Medium', 'nonarchival'),
+        'new_item_name'     => __('New Medium Name', 'nonarchival'),
+        'menu_name'         => __('Mediums', 'nonarchival'),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'show_in_rest'      => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'work/medium'),
+    );
+
+    register_taxonomy('medium', 'work', $args);
+}
+add_action('init', 'Nonarchival\FieldsToolkit\register_taxonomy_medium');
+
+// Seed default medium terms on plugin activation
+function activate() {
+    register_post_type_work();
+    register_taxonomy_medium();
+
+    $default_mediums = array(
+        'Film',
+        'Video',
+        'Sound',
+        'Photography',
+        'Drawing',
+        'Painting',
+        'Installation',
+        'Sculpture',
+        'Performance',
+        'Digital',
+        'Design'
+    );
+
+    foreach ($default_mediums as $medium) {
+        if (!term_exists($medium, 'medium')) {
+            wp_insert_term($medium, 'medium');
+        }
+    }
+
+    flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'Nonarchival\FieldsToolkit\activate');
